800174977a29933d2cd677faa7cdbaf39cde9e3a
George Maddocks <george.maddocks@beamery.com>
2022-01-01 14:19:35 +0000

    Day thirteen refactors

--- 13/main.go
+++ 13/main.go
@@ -18,19 +18,19 @@ func main() {
 
 	switch os.Args[1] {
 	case "1":
-		one(grid, maxX, maxY, folds)
+		one(grid, folds)
 	case "2":
 		two(grid, maxX, maxY, folds)
 	}
 }
 
-func one(grid grid, maxX int, maxY int, folds []foldInstructions) {
+func one(grid grid, folds []foldInstructions) {
 	fold := folds[0]
 	switch fold.axis {
 	case "x":
-		foldByX(grid, maxX, maxY, fold.line)
+		foldByX(grid, fold.line)
 	case "y":
-		foldByY(grid, maxX, maxY, fold.line)
+		foldByY(grid, fold.line)
 	}
 	fmt.Println("result:", len(grid))
 }
@@ -39,19 +39,42 @@ func two(grid grid, maxX int, maxY int, folds []foldInstructions) {
 	for _, fold := range folds {
 		switch fold.axis {
 		case "x":
-			foldByX(grid, maxX, maxY, fold.line)
+			foldByX(grid, fold.line)
 			maxX = fold.line - 1
 		case "y":
-			foldByY(grid, maxX, maxY, fold.line)
+			foldByY(grid, fold.line)
 			maxY = fold.line - 1
 		}
 	}
 	printCodeString(grid, maxX, maxY)
 }
 
+func foldByX(grid grid, foldLine int) {
+	for key := range grid {
+		x, y := splitCoords(key)
+		if x <= foldLine {
+			continue
+		}
+		delete(grid, key)
+		newCoords := getCoords(foldLine-(x-foldLine), y)
+		grid[newCoords] = struct{}{}
+	}
+}
+
+func foldByY(grid grid, foldLine int) {
+	for key := range grid {
+		x, y := splitCoords(key)
+		if y <= foldLine {
+			continue
+		}
+		delete(grid, key)
+		newCoords := getCoords(x, foldLine-(y-foldLine))
+		grid[newCoords] = struct{}{}
+	}
+}
+
 func printCodeString(grid grid, maxX int, maxY int) {
 	str := ""
-
 	for y := 0; y <= maxY; y++ {
 		for x := 0; x <= maxX; x++ {
 			if _, ok := grid[getCoords(x, y)]; ok {
@@ -65,30 +88,6 @@ func printCodeString(grid grid, maxX int, maxY int) {
 	fmt.Print(str)
 }
 
-func foldByX(grid grid, maxX int, maxY int, foldLine int) {
-	for x := foldLine + 1; x <= maxX; x++ {
-		for y := 0; y <= maxY; y++ {
-			if _, ok := grid[getCoords(x, y)]; ok {
-				delete(grid, getCoords(x, y))
-				newCoords := getCoords(foldLine-(x-foldLine), y)
-				grid[newCoords] = struct{}{}
-			}
-		}
-	}
-}
-
-func foldByY(grid grid, maxX int, maxY int, foldLine int) {
-	for x := 0; x <= maxX; x++ {
-		for y := foldLine + 1; y <= maxY; y++ {
-			if _, ok := grid[getCoords(x, y)]; ok {
-				delete(grid, getCoords(x, y))
-				newCoords := getCoords(x, foldLine-(y-foldLine))
-				grid[newCoords] = struct{}{}
-			}
-		}
-	}
-}
-
 type grid map[string]struct{}
 type foldInstructions struct {
 	axis string
@@ -99,15 +98,15 @@ func createGrid(points []string) (grid, int, int) {
 	grid := make(grid)
 	maxX := 0
 	maxY := 0
-	for _, point := range points {
-		splitPoint := strings.Split(point, ",")
-		if x, _ := strconv.Atoi(splitPoint[0]); x > maxX {
+	for _, coords := range points {
+		x, y := splitCoords(coords)
+		if x > maxX {
 			maxX = x
 		}
-		if y, _ := strconv.Atoi(splitPoint[1]); y > maxY {
+		if y > maxY {
 			maxY = y
 		}
-		grid[point] = struct{}{}
+		grid[coords] = struct{}{}
 	}
 
 	return grid, maxX, maxY
@@ -126,4 +125,11 @@ func parseFolds(folds []string) []foldInstructions {
 
 func getCoords(x, y int) string {
 	return fmt.Sprintf("%d,%d", x, y)
+}
+
+func splitCoords(coords string) (x, y int) {
+	splitCoords := strings.Split(coords, ",")
+	x, _ = strconv.Atoi(splitCoords[0])
+	y, _ = strconv.Atoi(splitCoords[1])
+	return x, y
 }
